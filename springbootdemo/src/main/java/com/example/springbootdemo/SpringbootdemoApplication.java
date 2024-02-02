6829dc54f11ced22a54024635b6a1cb483adb868
Antouela B <antoubi10@gmail.com>
2024-02-02 15:18:07 +0200

    Test commit

--- springbootdemo/src/main/java/com/example/springbootdemo/SpringbootdemoApplication.java
+++ springbootdemo/src/main/java/com/example/springbootdemo/SpringbootdemoApplication.java
@@ -8,6 +8,7 @@ public class SpringbootdemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootdemoApplication.class, args);
+		System.out.println("hello");
 	}
 
 	/*url for this db server http://localhost:8080/api/users*/
